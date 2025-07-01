e4193ae3c0d2c424c10cae380ccf5fe81497dafa
turyakiracresent <cresentturyakira@gmail.com>
2025-07-01 11:18:39 +0300

    I have added static files

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory where static files will be collected
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Directory for static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
